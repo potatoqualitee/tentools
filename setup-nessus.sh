b2625d9ba0820c79a8eeb73c951db4995c142cb5
Chrissy LeMaire <clemaire@gmail.com>
2020-09-23 13:15:30 +0200

    add user

--- setup-nessus.sh
+++ setup-nessus.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /opt/nessus/sbin/nessuscli fetch --register-offline /tmp/nessus.license
-/usr/bin/expect --help
+curl 'https://localhost:8834/users' -H 'Content-Type: application/json' --data-binary '{"username":"admin","password":"admin1","permissions":128}' --insecure
+/opt/nessus/sbin/nessuscli lsuser
