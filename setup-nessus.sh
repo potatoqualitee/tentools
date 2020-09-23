9762b5650c46e00d65e12429b896bd23323acdf7
Chrissy LeMaire <clemaire@gmail.com>
2020-09-23 13:18:13 +0200

    move setup

--- setup-nessus.sh
+++ setup-nessus.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
 /opt/nessus/sbin/nessuscli fetch --register-offline /tmp/nessus.license
-curl 'https://localhost:8834/users' -H 'Content-Type: application/json' --data-binary '{"username":"admin","password":"admin1","permissions":128}' --insecure
-/opt/nessus/sbin/nessuscli lsuser
