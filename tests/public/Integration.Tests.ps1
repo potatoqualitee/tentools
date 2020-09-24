668a6b6fae7417b938ad55cd3b1eb0f98ae65c88
Chrissy LeMaire <clemaire@gmail.com>
2020-09-24 19:46:23 +0200

    add test suite

---
+++ tests/public/Integration.Tests.ps1
@@ -0,0 +1,23 @@
+﻿Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\..\constants.ps1"
+
+Describe "Integration Tests" -Tag "IntegrationTests" {
+    BeforeAll {
+    }
+    AfterAll {
+    }
+
+    Context "Connect-TenServer" {
+        It "Connects to a site" {
+            $cred = New-Object -TypeName PSCredential -ArgumentList "admin", (ConvertTo-SecureString -String admin123 -AsPlainText -Force)
+            $splat = @{
+                ComputerName         = "localhost"
+                AcceptSelfSignedCert = $true
+                Credential           = $cred
+                EnableException      = $true
+                Port                 = 8834
+            }
+            Connect-TenServer @splat
+        }
+    }
+}
