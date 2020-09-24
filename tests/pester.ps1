e49600ba4d780e5e2db2686382b9349d89d79450
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2020-09-24 21:15:10 +0200

    Update tests (#12)

--- tests/pester.ps1
+++ tests/pester.ps1
@@ -7,7 +7,7 @@ Write-PSFMessage -Level Important -Message "Loading constants"
 . "$PSScriptRoot\constants.ps1"
 
 Write-PSFMessage -Level Important -Message "Importing Module"
-# Import-Module "$PSScriptRoot\..\tentools.psd1"
+Import-Module "$PSScriptRoot\..\tentools.psd1"
 
 $totalFailed = 0
 $totalRun = 0
@@ -34,7 +34,7 @@ foreach ($file in (Get-ChildItem "$PSScriptRoot\public" -Recurse -File -Filter "
     }
 }
 
-$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
+$null = $testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
 
 if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without failure" }
 else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed" }
--- tests/public/Integration.Tests.ps1
+++ tests/public/Integration.Tests.ps1
@@ -1,12 +1,12 @@
 ﻿Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\..\constants.ps1"
 
+
 Describe "Integration Tests" -Tag "IntegrationTests" {
     BeforeAll {
+        # Give it time to do whatever it needs to do
+        Start-Sleep 20
     }
-    AfterAll {
-    }
-
     Context "Connect-TenServer" {
         It "Connects to a site" {
             $cred = New-Object -TypeName PSCredential -ArgumentList "admin", (ConvertTo-SecureString -String admin123 -AsPlainText -Force)
@@ -18,6 +18,12 @@ Describe "Integration Tests" -Tag "IntegrationTests" {
                 Port                 = 8834
             }
             Connect-TenServer @splat
+        }
+    }
+
+    Context "Get-TenUser" {
+        It "Returns a user" {
+            Get-TenUser | Select-Object -ExpandProperty name | Should -Contain "admin"
         }
     }
 }
