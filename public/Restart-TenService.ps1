8016826ba19d906a866cc7c4136ab495962b2e9f
Chrissy LeMaire <clemaire@gmail.com>
2020-09-25 15:22:56 +0200

    add restart command

---
+++ public/Restart-TenService.ps1
@@ -0,0 +1,44 @@
+function Restart-TenService {
+    <#
+    .SYNOPSIS
+        Short description
+
+    .DESCRIPTION
+        Long description
+
+    .PARAMETER SessionId
+        ID of a valid Nessus session. This is auto-populated after a connection is made using Connect-TenServer.
+
+    .PARAMETER ScanId
+        Parameter description
+
+    .PARAMETER AlternateTarget
+        Parameter description
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .EXAMPLE
+        PS> Get-Ten
+    #>
+    [CmdletBinding()]
+    param
+    (
+        # Nessus session Id
+        [Parameter(Position = 0, ValueFromPipelineByPropertyName)]
+        [Alias('Index')]
+        [int32[]]$SessionId = $script:NessusConn.SessionId,
+        [switch]$EnableException
+    )
+    begin {
+        $params = @{ }
+        $paramJson = ConvertTo-Json -InputObject $params -Compress
+    }
+    process {
+        foreach ($session in (Get-TenSession -SessionId $SessionId)) {
+            Invoke-TenRequest -SessionObject $session -Path "/server/restart" -Method 'Post' -Parameter $paramJson -ContentType 'application/json'
+        }
+    }
+}
