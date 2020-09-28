2d26c0c71626c952c410a4b92061250282acf475
Chrissy LeMaire <clemaire@gmail.com>
2020-09-28 16:07:09 +0200

    add

---
+++ private/Invoke-NonAuthRequest.ps1
@@ -0,0 +1,62 @@
+function Invoke-NonAuthRequest {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory)]
+        [string]$ComputerName,
+        [Parameter(Mandatory)]
+        [string]$Path,
+        [int]$Port = "8834",
+        [String]$Method = "GET",
+        [String]$ContentType = "application/json",
+        [switch]$AcceptSelfSignedCert,
+        [switch]$EnableException
+    )
+    begin {
+        if ($PSVersionTable.PSEdition -eq 'Core') {
+            if ($AcceptSelfSignedCert) {
+                $PSDefaultParameterValues['Invoke-RestMethod:SkipCertificateCheck'] = $true
+            }
+        } else {
+            if ($AcceptSelfSignedCert -and [System.Net.ServicePointManager]::CertificatePolicy.ToString() -ne 'IgnoreCerts') {
+                $Domain = [AppDomain]::CurrentDomain
+                $DynAssembly = New-Object System.Reflection.AssemblyName('IgnoreCerts')
+                $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
+                $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('IgnoreCerts', $false)
+                $TypeBuilder = $ModuleBuilder.DefineType('IgnoreCerts', 'AutoLayout, AnsiClass, Class, Public, BeforeFieldInit', [System.Object], [System.Net.ICertificatePolicy])
+                $TypeBuilder.DefineDefaultConstructor('PrivateScope, Public, HideBySig, SpecialName, RTSpecialName') | Out-Null
+                $MethodInfo = [System.Net.ICertificatePolicy].GetMethod('CheckValidationResult')
+                $MethodBuilder = $TypeBuilder.DefineMethod($MethodInfo.Name, 'PrivateScope, Public, Virtual, HideBySig, VtableLayoutMask', $MethodInfo.CallingConvention, $MethodInfo.ReturnType, ([Type[]] ($MethodInfo.GetParameters() | ForEach-Object { $_.ParameterType })))
+                $ILGen = $MethodBuilder.GetILGenerator()
+                $ILGen.Emit([Reflection.Emit.Opcodes]::Ldc_I4_1)
+                $ILGen.Emit([Reflection.Emit.Opcodes]::Ret)
+                $TypeBuilder.CreateType() | Out-Null
+
+                # Disable SSL certificate validation
+                [System.Net.ServicePointManager]::CertificatePolicy = New-Object IgnoreCerts
+            }
+        }
+
+        # Force usage of TSL1.2 as Nessus web server only supports this and will hang otherwise
+        # Source: https://stackoverflow.com/questions/32355556/powershell-invoke-restmethod-over-https
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    }
+    process {
+        foreach ($computer in $ComputerName) {
+            $uri = "https://$($computer):$Port$Path"
+
+            $params = @{
+                Method      = $Method
+                ContentType = $ContentType
+                URI         = $uri
+            }
+
+            try {
+                Invoke-RestMethod @params -ErrorAction Stop
+            } catch {
+                $msg = Get-ErrorMessage -Record $_
+                Stop-PSFFunction -EnableException:$EnableException -Message "$msg $_" -ErrorRecord $_ -Continue
+            }
+        }
+    }
+}
--- public/Wait-TenServerReady.ps1
+++ public/Wait-TenServerReady.ps1
@@ -22,63 +22,21 @@ function Wait-TenServerReady {
     (
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
         [string[]]$ComputerName,
-        [int]$Port,
+        [int]$Port = "8834",
         [switch]$AcceptSelfSignedCert,
-        [ValidateSet("tenable.sc", "Nessus")]
-        [string]$Type,
         [int]$Timeout = 60,
         [switch]$EnableException
     )
-    begin {
-        if ($PSVersionTable.PSEdition -eq 'Core') {
-            if ($AcceptSelfSignedCert) {
-                $PSDefaultParameterValues['Invoke-RestMethod:SkipCertificateCheck'] = $true
-            }
-        } else {
-            if ($AcceptSelfSignedCert -and [System.Net.ServicePointManager]::CertificatePolicy.ToString() -ne 'IgnoreCerts') {
-                $Domain = [AppDomain]::CurrentDomain
-                $DynAssembly = New-Object System.Reflection.AssemblyName('IgnoreCerts')
-                $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
-                $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('IgnoreCerts', $false)
-                $TypeBuilder = $ModuleBuilder.DefineType('IgnoreCerts', 'AutoLayout, AnsiClass, Class, Public, BeforeFieldInit', [System.Object], [System.Net.ICertificatePolicy])
-                $TypeBuilder.DefineDefaultConstructor('PrivateScope, Public, HideBySig, SpecialName, RTSpecialName') | Out-Null
-                $MethodInfo = [System.Net.ICertificatePolicy].GetMethod('CheckValidationResult')
-                $MethodBuilder = $TypeBuilder.DefineMethod($MethodInfo.Name, 'PrivateScope, Public, Virtual, HideBySig, VtableLayoutMask', $MethodInfo.CallingConvention, $MethodInfo.ReturnType, ([Type[]] ($MethodInfo.GetParameters() | ForEach-Object { $_.ParameterType })))
-                $ILGen = $MethodBuilder.GetILGenerator()
-                $ILGen.Emit([Reflection.Emit.Opcodes]::Ldc_I4_1)
-                $ILGen.Emit([Reflection.Emit.Opcodes]::Ret)
-                $TypeBuilder.CreateType() | Out-Null
-
-                # Disable SSL certificate validation
-                [System.Net.ServicePointManager]::CertificatePolicy = New-Object IgnoreCerts
-            }
-        }
-
-        # Force usage of TSL1.2 as Nessus web server only supports this and will hang otherwise
-        # Source: https://stackoverflow.com/questions/32355556/powershell-invoke-restmethod-over-https
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-        if (-Not $Type) {
-            if ($Port -eq 443) {
-                $Type = "tenable.sc"
-            } else {
-                $Type = "Nessus"
-            }
-        }
-
-        if ($Type -and -not $Port) {
-            if ($Type -eq "tenable.sc") {
-                $Port = "443"
-            } else {
-                $Port = "8834"
-            }
-        }
-    }
     process {
         foreach ($computer in $ComputerName) {
+            $params = @{
+                ComputerName    = $computer
+                Port            = $Port
+                Path            = "/server/status"
+                EnableException = $EnableException
+            }
             do {
-                $uri = "https://$($computer):$Port/server/status"
-                $result = Invoke-RestMethod -Uri $uri
+                $result = Invoke-NonAuthRequest @params
                 Start-Sleep 1
                 $i++
             }
