bbce7b3739f842654aebb6ee4d47a0b6d18dde90
Chrissy LeMaire <clemaire@gmail.com>
2020-10-03 15:35:09 +0200

    Update Integration.Tests.ps1

--- tests/public/Integration.Tests.ps1
+++ tests/public/Integration.Tests.ps1
@@ -1,17 +1,12 @@
 ﻿Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\..\constants.ps1"
 
-
 Describe "Integration Tests" -Tag "IntegrationTests" {
-    BeforeAll {
-        # Give it time to do whatever it needs to do
-        Wait-TenServerReady -ComputerName localhost
-    }
     BeforeEach {
-        Write-Output -Message "Next test"
+        Write-Warning -Message "Next test"
     }
-    Context "Connect-TenServer" {
-        It "Connects to a site" {
+    Context "All commands" {
+        It "Connect-TenServer connects to a site" {
             $cred = New-Object -TypeName PSCredential -ArgumentList "admin", (ConvertTo-SecureString -String admin123 -AsPlainText -Force)
             $splat = @{
                 ComputerName         = "localhost"
@@ -22,20 +17,15 @@ Describe "Integration Tests" -Tag "IntegrationTests" {
             }
             (Connect-TenServer @splat).ComputerName | Should -Be "localhost"
         }
-    }
 
-    Context "Get-TenUser" {
-        It "Returns a user" {
+        It "Get-TenUser returns a user" {
             Get-TenUser | Select-Object -ExpandProperty name | Should -Contain "admin"
         }
-    }
-    Context "Get-TenFolder" {
-        It "Returns a folder" {
+        It "Get-TenFolder a folder" {
             Get-TenFolder | Select-Object -ExpandProperty name | Should -Contain "Trash"
         }
-    }
-    Context "Get-TenPlugin" {
-        It "Returns proper plugin information" {
+
+        It "Get-TenPlugin returns proper plugin information" {
             $results = Get-TenPlugin -PluginId 100000
             $results | Select-Object -ExpandProperty Name | Should -Be 'Test Plugin for tentools'
             $results | Select-Object -ExpandProperty PluginId | Should -Be 100000
