cc26278ac8b2a6078bc0200ade6803c3d4c6623b
Chrissy LeMaire <clemaire@gmail.com>
2020-10-20 15:26:13 +0200

    add built-in support for sessions

--- public/Invoke-TenRequest.ps1
+++ public/Invoke-TenRequest.ps1
@@ -2,11 +2,10 @@ function Invoke-TenRequest {
     [CmdletBinding()]
     Param
     (
+        [PSCustomObject[]]$SessionObject = (Get-TenSession),
+        [String]$Parameter,
         [Parameter(Mandatory)]
-        $SessionObject,
-        $Parameter,
-        [Parameter(Mandatory)]
-        [string]$Path,
+        [String]$Path,
         [Parameter(Mandatory)]
         [String]$Method,
         [String]$OutFile,
@@ -15,110 +14,111 @@ function Invoke-TenRequest {
         [switch]$EnableException
 
     )
-    begin {
-        # to manage differences between nessus and tenable.sc
-        if ($SessionObject.sc) {
-            $replace = @{
-                "/plugins/families/" = "/pluginFamily/"
-            }
-
-            foreach ($key in $replace.keys) {
-                $Path = $Path.Replace($key, $replace[$key])
-            }
-
-            if ($Path -match '/group/' -and $Path -match '/user') {
-                $Path = $Path.Replace("/user", "?fields=users")
-            }
-
-            if ($Path -match '/policies') {
-                if ($Path -notmatch '/policies/') {
-                    $Path = $Path.Replace("/policies", "/policy?filter=*&fields=name,description,tags,type,createdTime,ownerGroup,groups,owner,modifiedTime,policyTemplate,canUse,canManage,status")
-                } else {
-                    $id = Split-path $Path -Leaf
-                    $Path = $Path.Replace("/policies/", "/policy/$($id)?fields=name,description,tags,type,createdTime,ownerGroup,groups,owner,modifiedTime,policyTemplate,canUse,canManage,status")
-                }
-            }
-
-            if ($Path -match '/editor/policy') {
-                if ($Path -notmatch '/policies/') {
-                    $Path = $Path.Replace("/editor/policy", "/policy?filter=*&fields=name,description,tags,type,createdTime,ownerGroup,groups,owner,modifiedTime,policyTemplate,canUse,canManage,status")
-                } else {
-                    $id = Split-path $Path -Leaf
-                    $Path = $Path.Replace("/editor/policy/", "/policy/$($id)?fields=name,description,tags,type,createdTime,ownerGroup,groups,owner,modifiedTime,policyTemplate,canUse,canManage,status")
-                }
-            }
-        }
-    }
     process {
-        if ($SessionObject.sc -and $Path -eq "/server/properties") {
-            return $null
-        }
+        foreach ($session in $SessionObject) {
+            # to manage differences between nessus and tenable.sc
+            if ($session.sc) {
+                $replace = @{
+                    "/plugins/families/" = "/pluginFamily/"
+                }
 
-        if ($SessionObject.sc) {
-            $headers = @{
-                "X-SecurityCenter" = $SessionObject.Token
+                foreach ($key in $replace.keys) {
+                    $Path = $Path.Replace($key, $replace[$key])
+                }
+
+                if ($Path -match '/group/' -and $Path -match '/user') {
+                    $Path = $Path.Replace("/user", "?fields=users")
+                }
+
+                if ($Path -match '/policies') {
+                    if ($Path -notmatch '/policies/') {
+                        $Path = $Path.Replace("/policies", "/policy?filter=*&fields=name,description,tags,type,createdTime,ownerGroup,groups,owner,modifiedTime,policyTemplate,canUse,canManage,status")
+                    } else {
+                        $id = Split-path $Path -Leaf
+                        $Path = $Path.Replace("/policies/", "/policy/$($id)?fields=name,description,tags,type,createdTime,ownerGroup,groups,owner,modifiedTime,policyTemplate,canUse,canManage,status")
+                    }
+                }
+
+                if ($Path -match '/editor/policy') {
+                    if ($Path -notmatch '/policies/') {
+                        $Path = $Path.Replace("/editor/policy", "/policy?filter=*&fields=name,description,tags,type,createdTime,ownerGroup,groups,owner,modifiedTime,policyTemplate,canUse,canManage,status")
+                    } else {
+                        $id = Split-path $Path -Leaf
+                        $Path = $Path.Replace("/editor/policy/", "/policy/$($id)?fields=name,description,tags,type,createdTime,ownerGroup,groups,owner,modifiedTime,policyTemplate,canUse,canManage,status")
+                    }
+                }
             }
-        } else {
-            $headers = @{
-                "X-Cookie" = "token=$($SessionObject.Token)"
+
+            if ($session.sc -and $Path -eq "/server/properties") {
+                return $null
             }
-        }
-        $RestMethodParams = @{
-            Method          = $Method
-            'URI'           = "$($SessionObject.URI)$($Path)"
-            'Headers'       = $headers
-            'ErrorVariable' = 'NessusUserError'
-            'WebSession'    = $SessionObject.WebSession
-        }
 
-        if ($Parameter) {
-            $RestMethodParams.Add('Body', $Parameter)
-        }
+            if ($session.sc) {
+                $headers = @{
+                    "X-SecurityCenter" = $session.Token
+                }
+            } else {
+                $headers = @{
+                    "X-Cookie" = "token=$($session.Token)"
+                }
+            }
+            $RestMethodParams = @{
+                Method          = $Method
+                'URI'           = "$($session.URI)$($Path)"
+                'Headers'       = $headers
+                'ErrorVariable' = 'NessusUserError'
+                'WebSession'    = $session.WebSession
+            }
 
-        if ($OutFile) {
-            $RestMethodParams.add('OutFile', $OutFile)
-        }
+            if ($Parameter) {
+                $RestMethodParams.Add('Body', $Parameter)
+            }
 
-        if ($ContentType) {
-            $RestMethodParams.add('ContentType', $ContentType)
-        }
+            if ($OutFile) {
+                $RestMethodParams.add('OutFile', $OutFile)
+            }
 
-        if ($InFile) {
-            $RestMethodParams.add('InFile', $InFile)
-        }
+            if ($ContentType) {
+                $RestMethodParams.add('ContentType', $ContentType)
+            }
 
-        try {
-            #$RestMethodParams.Uri
-            Write-PSFMessage -Level Verbose -Message "Connecting to $($SessionObject.URI)"
-            $results = Invoke-RestMethod @RestMethodParams -ErrorAction Stop
-        } catch [Net.WebException] {
-            [int]$res = $_.Exception.Response.StatusCode
-            if ($res -eq 401) {
-                # Request failed. More than likely do to time-out.
-                # Re-Authenticating using information from session.
-                Write-PSFMessage -Level Verbose -Message 'The session has expired, Re-authenticating'
-                try {
-                    $null = $script:NessusConn.Remove($SessionObject)
-                    $bound = $SessionObject.Bound
-                    $null = Connect-TenServer @bound
-                    $results = Invoke-RestMethod @RestMethodParams -ErrorAction Stop
-                } catch {
+            if ($InFile) {
+                $RestMethodParams.add('InFile', $InFile)
+            }
+
+            try {
+                #$RestMethodParams.Uri
+                Write-PSFMessage -Level Verbose -Message "Connecting to $($session.URI)"
+                $results = Invoke-RestMethod @RestMethodParams -ErrorAction Stop
+            } catch [Net.WebException] {
+                [int]$res = $_.Exception.Response.StatusCode
+                if ($res -eq 401) {
+                    # Request failed. More than likely do to time-out.
+                    # Re-Authenticating using information from session.
+                    Write-PSFMessage -Level Verbose -Message 'The session has expired, Re-authenticating'
+                    try {
+                        $null = $script:NessusConn.Remove($session)
+                        $bound = $session.Bound
+                        $null = Connect-TenServer @bound
+                        $results = Invoke-RestMethod @RestMethodParams -ErrorAction Stop
+                    } catch {
+                        $msg = Get-ErrorMessage -Record $_
+                        Stop-PSFFunction -EnableException:$EnableException -Message $msg -ErrorRecord $_ -Continue
+                    }
+                } else {
                     $msg = Get-ErrorMessage -Record $_
                     Stop-PSFFunction -EnableException:$EnableException -Message $msg -ErrorRecord $_ -Continue
                 }
-            } else {
+            } catch {
                 $msg = Get-ErrorMessage -Record $_
                 Stop-PSFFunction -EnableException:$EnableException -Message $msg -ErrorRecord $_ -Continue
             }
-        } catch {
-            $msg = Get-ErrorMessage -Record $_
-            Stop-PSFFunction -EnableException:$EnableException -Message $msg -ErrorRecord $_ -Continue
-        }
 
-        if ($results.response) {
-            $results.response
-        } else {
-            $results
+            if ($results.response) {
+                $results.response
+            } else {
+                $results
+            }
         }
     }
 }
