e95cbca1c0717a30b6ca3b12e2a38dde26872388
Chrissy LeMaire <clemaire@gmail.com>
2020-11-17 11:00:52 +0100

    Get-TNReportAttribute

---
+++ public/Get-TNReportAttribute.ps1
@@ -0,0 +1,50 @@
+function Get-TNReportAttribute {
+    <#
+    .SYNOPSIS
+        Adds an organization
+
+    .DESCRIPTION
+        Adds an organization
+
+    .PARAMETER Name
+        Parameter description
+
+    .PARAMETER ZoneSelection
+        Parameter description
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .EXAMPLE
+        PS>  Get-TNRepository
+
+    #>
+    [CmdletBinding()]
+    param
+    (
+        [object[]]$SessionObject = (Get-TNSession),
+        [string[]]$Name,
+        [switch]$EnableException
+    )
+    process {
+        foreach ($session in $SessionObject) {
+            if (-not $session.sc) {
+                Stop-PSFFunction -Message "Only tenable.sc supported" -Continue
+            }
+
+            $params = @{
+                Path            = "/attributeSet?fields=attributes,createdTime,creator,description,modifiedTime,name,type"
+                Method          = "GET"
+                EnableException = $EnableException
+            }
+
+            if ($PSBoundParameters.Name) {
+                Invoke-TNRequest @params | ConvertFrom-TNRestResponse | Where-Object Name -in $Name
+            } else {
+                Invoke-TNRequest @params | ConvertFrom-TNRestResponse
+            }
+        }
+    }
+}
