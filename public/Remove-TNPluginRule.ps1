36b213891a1bc2f91d3bf9ae1e82793d8978e1e0
Chrissy LeMaire <clemaire@gmail.com>
2020-12-03 00:46:40 +0100

    Update Remove-TNPluginRule.ps1

--- public/Remove-TNPluginRule.ps1
+++ public/Remove-TNPluginRule.ps1
@@ -1,37 +1,40 @@
-function Remove-TNPluginRule {
+﻿function Remove-TNPluginRule {
     <#
     .SYNOPSIS
-        Removes a Nessus plugin rule
+        Removes a list of plugin rules
 
     .DESCRIPTION
+        Removes a list of plugin rules
+
         Can be used to clear a previously defined, scan report altering rule
 
+    .PARAMETER SessionObject
+        Optional parameter to force using specific SessionObjects. By default, each command will connect to all connected servers that have been connected to using Connect-TNServer
+
     .PARAMETER RuleId
-        RuleId number of the rule which would you like removed/deleted
+        The ID of the target rule
 
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+        This avoids overwhelming you with 'sea of red' exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this 'nice by default' feature off and enables you to catch exceptions with your own try/catch.
 
     .EXAMPLE
-        Remove-TNPluginRule -RuleId 500
+        PS C:\> Remove-TNPluginRule -RuleId 500
+
         Will delete a plugin rule with an RuleId of 500
 
     .EXAMPLE
-        Get-TNPluginRule | Remove-TNPluginRule
+        PS C:\> Get-TNPluginRule | Remove-TNPluginRule
+
         Will delete all rules
 
     .EXAMPLE
-        Get-TNPluginRule | ? {$_.Host -eq 'myComputer'} | Remove-TNPluginRule
+        PS C:\> Get-TNPluginRule | ? {$_.Host -eq 'myComputer'} | Remove-TNPluginRule
+
         Will find all plugin rules that match the computer name, and delete them
 
-    .INPUTS
-        Can accept pipeline data from Get-TNPluginRule
-
-    .OUTPUTS
-        Empty, unless an error is received from the server
-    #>
+#>
     [CmdletBinding()]
     param
     (
