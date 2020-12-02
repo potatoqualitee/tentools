a96a66e8384a9b247e9ab2036e629ec275c1676e
Chrissy LeMaire <clemaire@gmail.com>
2020-12-03 00:25:45 +0100

    Update Get-TNScanner.ps1

--- public/Get-TNScanner.ps1
+++ public/Get-TNScanner.ps1
@@ -1,26 +1,28 @@
-function Get-TNScanner {
-    <#
+﻿function Get-TNScanner {
+<#
     .SYNOPSIS
-        Gets a scanner
+        Gets a list of scanners
 
     .DESCRIPTION
-        Gets a scanner
-
+        Gets a list of scanners
+        
+    .PARAMETER SessionObject
+        Optional parameter to force using specific SessionObjects. By default, each command will connect to all connected servers that have been connected to using Connect-TNServer
+        
     .PARAMETER Name
-        Parameter description
-
-    .PARAMETER ZoneSelection
-        Parameter description
-
+        The name of the target scanner
+        
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-
+        This avoids overwhelming you with 'sea of red' exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this 'nice by default' feature off and enables you to catch exceptions with your own try/catch.
+        
     .EXAMPLE
-        PS>  New-TNOrganization -Name "Acme Corp"
+        PS C:\> Get-TNScanner
 
-    #>
+        Gets a list of scanners
+        
+#>
     [CmdletBinding()]
     param
     (
