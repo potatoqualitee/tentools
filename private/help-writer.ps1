9d484e58f9fa44e2ab17ccc2a3d199e09b3c9aa2
Chrissy LeMaire <clemaire@gmail.com>
2020-12-02 23:49:16 +0100

    Update help-writer.ps1

--- private/help-writer.ps1
+++ private/help-writer.ps1
@@ -11,10 +11,11 @@ Function Write-Help {
         $noun = $command.Noun.TrimStart("TN")
         $noun = ($noun -csplit "([A-Z][a-z]+)" | Where-Object { $_ }) -join " "
         $noun = $noun.ToLower()
+        $params = $command.parameters.keys | Where-Object { $_ -notin [System.Management.Automation.PSCmdlet]::CommonParameters }
 
         switch ($verb) {
             "Add" {
-                $synopsis = "$($verb)s a $($noun)s"
+                $synopsis = "$($verb)s a list of $($noun.ToLower())s"
             }
             "Connect" {
                 $synopsis = "$($verb)s to a Nessus or tenable.sc server"
@@ -34,15 +35,104 @@ Function Write-Help {
             "Wait" {
                 $synopsis = "Waits for a Nessus server to be ready"
             }
+            "Set" {
+                $synopsis = "Sets properties for $($noun)s"
+            }
             default {
                 # Get, Disable, Edits, Enables, Exports
                 $synopsis = "$($verb)s a list of $($noun)s"
             }
         }
 
-        "    .SYNOPSIS"
-        "        $synopsis"
+        "    .SYNOPSIS
+        $synopsis
 
+    .DESCRIPTION
+        $synopsis
+        "
+
+        foreach ($param in $params) {
+            switch ($param) {
+                "EnableException" {
+                    $description = "By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with 'sea of red' exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this 'nice by default' feature off and enables you to catch exceptions with your own try/catch."
+                }
+                "ComputerName" {
+                    $description = "The network name or IP address of the Nessus or tenable.sc server"
+                }
+                "Port" {
+                    $description = "The port of the Nessus or tenable.sc server. Defaults to 8834 which is the default port for Nessus."
+                }
+                "Credential" {
+                    $description = "The credential object (from Get-Credential) used to log into the target server. Specifies a user account that has permission to send the request. "
+                }
+                "UseDefaultCredential" {
+                    $description = "Indicates that the command uses the credentials of the current user to send the web request. This can't be used with Authentication or Credential and may not be supported on all platforms."
+                }
+                "AcceptSelfSignedCert" {
+                    $description = "Accept self-signed certs"
+                }
+                "SessionObject" {
+                    $description = "Optional parameter to force using specific SessionObjects. By default, each command will connect to all connected servers that have been connected to using Connect-TNServer"
+                }
+                "PolicyId" {
+                    $description = "The ID of the target policy"
+                }
+                "PolicyUUID" {
+                    $description = "The UUID of the target policy"
+                }
+                "PluginId" {
+                    $description = "The ID of the target plugin"
+                }
+                "FamilyId" {
+                    $description = "The ID of the target family"
+                }
+                "ScanId" {
+                    $description = "The ID of the target scan"
+                }
+                "UserId" {
+                    $description = "The ID of the target user"
+                }
+                "GroupId" {
+                    $description = "The ID of the target group"
+                }
+                "RuleId" {
+                    $description = "The ID of the target rule"
+                }
+                "Email" {
+                    $description = "The email address of the target user"
+                }
+                "Name" {
+                    $description = "The name of the target $($noun.ToLower())"
+                }
+                "Type" {
+                    $description = "The type of $($noun.ToLower())"
+                }
+                "FileName" {
+                    $description = "The filename of the target $($noun.ToLower())"
+                }
+                "Organization" {
+                    $description = "The name of the target organization"
+                }
+                "Password" {
+                    $description = "The required password. This is a securestring type. The easiest way to get this is by using (Get-Credential).Password which extracts the password in a secure manner (and does not care about the username.)"
+                }
+                default {
+                    $description = "Description for $param"
+                }
+            }
+
+            "    .PARAMETER $param
+        $description
+        "
+        }
+
+        "    .EXAMPLE
+        PS C:\> $name
+
+        $synopsis
+        "
     }
 
     function Get-Body ($text) {
