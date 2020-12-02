100347e258c9cc01f8505b9fd1380a912066a75f
Chrissy LeMaire <clemaire@gmail.com>
2020-12-03 00:24:13 +0100

    Update Get-TNFolder.ps1

--- public/Get-TNFolder.ps1
+++ public/Get-TNFolder.ps1
@@ -1,37 +1,25 @@
-function Get-TNFolder {
-    <#
-    .Synopsis
-        Gets folders configured on a Nessus Server.
+﻿function Get-TNFolder {
+<#
+    .SYNOPSIS
+        Gets a list of folders
 
     .DESCRIPTION
-        Gets folders configured on a Nessus Server.
-
+        Gets a list of folders
+        
+    .PARAMETER SessionObject
+        Optional parameter to force using specific SessionObjects. By default, each command will connect to all connected servers that have been connected to using Connect-TNServer
+        
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-
+        This avoids overwhelming you with 'sea of red' exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this 'nice by default' feature off and enables you to catch exceptions with your own try/catch.
+        
     .EXAMPLE
-        Get-TNFolder
+        PS C:\> Get-TNFolder
 
-        Name    : My Scans
-        Id      : 2
-        Type    : main
-        Default : 1
-        Unread  : 5
-
-        Name    : Trash
-        Id      : 3
-        Type    : trash
-        Default : 0
-        Unread  :
-
-        Name    : Test Folder 2
-        Id      : 10
-        Type    : custom
-        Default : 0
-        Unread  : 0
-    #>
+        Gets a list of folders
+        
+#>
     [CmdletBinding()]
     param
     (
