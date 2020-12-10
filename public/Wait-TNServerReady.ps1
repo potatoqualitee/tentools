065ecd2182b4b1bfc67412db3b19c19c722019e0
Chrissy LeMaire <clemaire@gmail.com>
2020-12-10 11:05:52 +0100

    add example

--- public/Wait-TNServerReady.ps1
+++ public/Wait-TNServerReady.ps1
@@ -1,39 +1,39 @@
 ﻿function Wait-TNServerReady {
-<#
+    <#
     .SYNOPSIS
         Waits for a Nessus server to be ready
 
     .DESCRIPTION
         Waits for a Nessus server to be ready
-        
+
     .PARAMETER ComputerName
         The network name or IP address of the Nessus or tenable.sc server
-        
+
     .PARAMETER Port
         The port of the Nessus or tenable.sc server. Defaults to 8834 which is the default port for Nessus.
-        
+
     .PARAMETER AcceptSelfSignedCert
         Accept self-signed certs
-        
+
     .PARAMETER Register
         Description for Register
-        
+
     .PARAMETER Timeout
         Description for Timeout
-        
+
     .PARAMETER SilentUntil
         Description for SilentUntil
-        
+
     .PARAMETER EnableException
         By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
         This avoids overwhelming you with 'sea of red' exceptions, but is inconvenient because it basically disables advanced scripting.
         Using this switch turns this 'nice by default' feature off and enables you to catch exceptions with your own try/catch.
-        
-    .EXAMPLE
-        PS C:\> Wait-TNServerReady
 
-        Waits for a Nessus server to be ready
-        
+    .EXAMPLE
+        PS C:\> $null = Wait-TNServerReady -ComputerName nessus -AcceptSelfSignedCert:$AcceptSelfSignedCert
+
+        Waits for a Nessus server to be ready for a maximum of 60 seconds
+
 #>
     [CmdletBinding()]
     param
