82197a81296b6d912b1f3af6e8146052788c89fc
Chrissy LeMaire <clemaire@gmail.com>
2021-01-23 13:26:44 +0100

    dashboard

---
+++ public/Get-TNDashboard.ps1
@@ -0,0 +1,54 @@
+function Get-TNDashboard {
+    <#
+    .SYNOPSIS
+        Gets a list of dashboards
+
+    .DESCRIPTION
+        Gets a list of dashboards
+
+    .PARAMETER SessionObject
+        Optional parameter to force using specific SessionObjects. By default, each command will connect to all connected servers that have been connected to using Connect-TNServer
+
+    .PARAMETER Name
+        The name of the target asset
+
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with 'sea of red' exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this 'nice by default' feature off and enables you to catch exceptions with your own try/catch.
+
+    .EXAMPLE
+        PS C:\> Get-TNDashboard
+
+        Gets a list of dashboards
+
+#>
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [object[]]$SessionObject = (Get-TNSession),
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string[]]$Name,
+        [switch]$EnableException
+    )
+    process {
+        foreach ($session in $SessionObject) {
+            if (-not $session.sc) {
+                Stop-PSFFunction -EnableException:$EnableException -Message "Only tenable.sc supported" -Continue
+            }
+
+            $params = @{
+                Path            = "/dashboard?filter=usable&fields=name,description,activated,order,ownerGroup,groups,failedComponentCount,owner,targetGroup,status,tags,creator,canUse,canManage,createdTime,modifiedTime"
+                Method          = "GET"
+                EnableException = $EnableException
+            }
+
+            if ($PSBoundParameters.Name) {
+                Invoke-TNRequest @params | ConvertFrom-TNRestResponse | Where-Object Name -in $Name
+            } else {
+                Invoke-TNRequest @params | ConvertFrom-TNRestResponse
+            }
+        }
+    }
+}
