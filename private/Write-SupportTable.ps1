2f1a13b2db3e0b3531658cbb36e46344693b6f2e
Chrissy LeMaire <clemaire@gmail.com>
2021-01-25 09:50:54 +0100

    fix support table

--- private/Write-SupportTable.ps1
+++ private/Write-SupportTable.ps1
@@ -4,9 +4,9 @@ Function Write-SupportTable {
 
     $files = Get-ChildItem -Recurse C:\github\tentools\public\*.ps1
 
-    foreach ($file in $files.FullName) {
+    foreach ($file in $files) {
         $name = $file.basename
-        $text = ($file | Get-Content -Raw).Trim()
+        $text = (Get-ChildItem $file | Get-Content -Raw).Trim()
 
         if ($text -match "tenable.sc not supported") {
             "| $name | x | |"
